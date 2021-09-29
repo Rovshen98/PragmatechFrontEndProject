db5bf0ac9af8f9bd7d7764f0e581b48028d6c920
Rovshen98 <rovshens@bk.ru>
2021-09-29 20:41:36 +0400

    animation

---
+++ UI/animation_1/app.js
@@ -0,0 +1,20 @@
+var mains = document.querySelectorAll('.main');
+var buton= document.querySelectorAll(".btn")
+function anima(){
+[].forEach.call(mains, function(main) {
+  // do whatever
+  main.style.display="block";
+  main.style.opacity="0"
+
+});
+
+};
+document.body.addEventListener('click',(e)=> {
+    if (e.target.className!=="btn"){
+    [].forEach.call(mains, function(main) {
+        // do whatever
+        main.style.display="none";
+        main.style.opacity="0"
+      
+      });}      
+})
