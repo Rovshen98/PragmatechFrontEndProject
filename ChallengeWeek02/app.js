ecd84142b74d6492c4b35519afd8050276b10afd
Rovshen98 <rovshens@bk.ru>
2021-10-28 23:03:50 +0400

    stickynote

---
+++ ChallengeWeek02/app.js
@@ -0,0 +1,86 @@
+let button = document.querySelector(".btn")
+let hide = document.querySelector(".fa-times-circle")
+let note = document.querySelector(".note")
+button.addEventListener("click", ()=>{
+    note.style.display="flex";
+})
+hide.addEventListener("click", ()=>{
+    note.style.display="none";
+})
+
+// -------------------------------------------------
+
+let main = document.querySelector(".main");
+
+
+
+
+var input = document.querySelector("textarea");
+// color-----------------------
+var i = 0;    
+var color = ["#c2ff3d","#3dc2ff","#04e022","#bc83e6","#ebb328","red","aqua"];
+    
+    
+// z-index---------
+var x = 1
+
+
+main.addEventListener("click", ()=>{
+    console.log("koko")
+});
+
+
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+    //    ------------------
+        var a = ["-2deg", "-1deg","-8deg","-5deg","-6deg","2deg"];
+        var randomValue = a[Math.floor(a.length * Math.random())];
+    // +++++++++++++++++++++++++++++++++++
+    // ///////////////////////
+        i++
+        if (i===7){
+            i=0;
+        }
+        
+        
+        x++
+   
+    
+
+    // ++++++++++++++++++++++
+        var div = document.createElement('div');
+        div.classList.add("item");
+        var text = document.createElement("h1");
+        text.innerText= input.value;
+        div.style.backgroundColor = color[i];
+        div.style.width =  "300px";
+        div.style.height =  "300px";
+        div.style.transition =  "2s";
+        div.style.cursor =  "pointer";
+        div.style.overflowWrap= "break-word";
+        div.style.boxShadow= "0px 10px 24px 0px rgba(0,0,0,0.75)";
+        div.style.transform = "rotate(" + randomValue + ")";
+        div.style.margin= "5px";
+        div.style.padding= "10px 15px";
+        div.style.zIndex = x;
+        text.style.fontSize="1.5rem"
+        text.style.height= "100%"
+        main.appendChild(div);
+        div.appendChild(text);
+
+        
+   
+  }
+
+});
+
+
+
+
+
+ 
+
+
+
