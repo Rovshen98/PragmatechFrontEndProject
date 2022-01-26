67630d35a97a670c25e3f1e1b7f447d5f1eebb8a
Rovshen98 <rovshens@bk.ru>
2022-01-26 18:40:57 +0400

    profile

---
+++ REACT/profile/reactapp/src/User.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useParams, useResolvedPath } from 'react-router-dom';
+import {data} from './App';
+
+export default function User() {
+ const {id} = useParams()
+ data.map((user)=>{
+   
+          data.find(id => id =={id})
+  
+ })
+   
+   
+  
+ 
+ 
+
+  
+}
+
+
